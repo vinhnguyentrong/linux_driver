257fc9fa385f4abfc4850fa5427a820636820c04
Vinh Nguyen Trong <vinhnt2410@gmail.com>
2020-06-10 21:13:51 +0700

    [vir driver] create device file

--- kernel/src/virtual_driver.c
+++ kernel/src/virtual_driver.c
@@ -10,7 +10,7 @@
 
 #include <linux/module.h>
 #include <linux/fs.h>
-
+#include <linux/device.h>
 
 /*****************************************************************************/
 /** 
@@ -20,6 +20,8 @@
 typedef struct 
 {
   dev_t dev_num;      // device number
+  struct class *dev_class;
+  struct device *dev;
 } DriverInfo_t;
 
 DriverInfo_t gDriverInfo;
@@ -32,22 +34,45 @@ static int __init virtual_driver_init(void)
   uint32_t ret = 0;
   gDriverInfo.dev_num =MKDEV(235, 0);
   // ret = register_chrdev_region(gDriverInfo.dev_num, 1, "virtual_char_device");
-ret = alloc_chrdev_region(&gDriverInfo.dev_num, 0, 1, "virtual_char_device");
+  ret = alloc_chrdev_region(&gDriverInfo.dev_num, 0, 1, "virtual_char_device");
   if (ret < 0)
   {
     printk("Failed to register device number.\n");
     goto failed_register_devnum;
   }
+
+  /* Create device file */
+  gDriverInfo.dev_class = class_create(THIS_MODULE, "virtul_device_class");
+  if(gDriverInfo.dev_class == NULL)
+  {
+    printk("Failed to create a device class.\n");
+    goto failed_create_class;
+  }
+  gDriverInfo.dev = device_create(gDriverInfo.dev_class, 
+        NULL, gDriverInfo.dev_num, NULL, "virtual_device");
+  if(gDriverInfo.dev == NULL)
+  {
+    printk("Failed to create a device.\n");
+    goto failed_create_device;
+  }
+
   printk("Initialize virtual driver successfully.\n");
 
   return 0;
 
+failed_create_device:
+  device_destroy(gDriverInfo.dev_class, gDriverInfo.dev_num);
+failed_create_class:
+  class_destroy(gDriverInfo.dev_class);
 failed_register_devnum:
   return ret; 
 }
 
 static void __exit virtual_driver_exit(void)
 {
+  /* delete device file */
+  device_destroy(gDriverInfo.dev_class, gDriverInfo.dev_num);
+  class_destroy(gDriverInfo.dev_class);
   /* unregister device number */
   unregister_chrdev_region(gDriverInfo.dev_num, 1);
 
